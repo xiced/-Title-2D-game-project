3a451d6e54ab9c30970b7de7b3d7058971f646e0
xIced <raimail3y@gmail.com>
2020-05-10 22:37:07 -0400

    Week 2 - Heatlh & Score

---
+++ Project Major/Assets/TryoutFolder/TryoutScripts/CharacterMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private GameManager code;
+
+    //For movement
+    [SerializeField] private float jumpForce = 0f;
+    [SerializeField] private float speed = 5f;
+    private Vector3 Velocity = Vector3.zero;
+    private Rigidbody2D myRigidbody2D;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        code = GameObject.Find("gameManager").GetComponent<GameManager>();
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Movement();
+        Jump();
+    }
+
+    void Movement()
+    {
+        //Check if player move right
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            myRigidbody2D.velocity = new Vector2(speed, myRigidbody2D.velocity.y);
+        }
+        //Check if player move left
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            myRigidbody2D.velocity = new Vector2(-speed, myRigidbody2D.velocity.y);
+        }
+        //else
+        //{
+        //    myRigidbody2D.velocity = new Vector2(0, myRigidbody2D.velocity.y);
+        //}
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            myRigidbody2D.velocity = new Vector2(myRigidbody2D.velocity.x, jumpForce);
+
+        }
+    }
+
+}
---
+++ Project Major/Assets/TryoutFolder/TryoutScripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    //For scoring system
+    [SerializeField] private Text score;
+    private int points = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start at 0
+        score.text = "Score: " + points.ToString("D3");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddPoints()
+    {
+        points += 100;
+        score.text = "Score: " + points.ToString("D3");
+    }
+
+    public void LosePoints()
+    {
+        points -= 25;
+        score.text = "Score: " + points.ToString("D3");
+    }
+}
