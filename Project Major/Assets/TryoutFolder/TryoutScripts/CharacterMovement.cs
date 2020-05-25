d3d8b368a33245fda3674a3eeaaa7df19e796058
xIced <raimail3y@gmail.com>
2020-05-24 22:10:40 -0400

    Week 4 - Pause Game + Updated Movement

--- Project Major/Assets/TryoutFolder/TryoutScripts/CharacterMovement.cs
+++ Project Major/Assets/TryoutFolder/TryoutScripts/CharacterMovement.cs
@@ -11,7 +11,8 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private float speed = 5f;
     private Vector3 Velocity = Vector3.zero;
     private Rigidbody2D myRigidbody2D;
-
+    private float moveInput;
+    private bool lookright = true;
     
 
     // Start is called before the first frame update
@@ -21,13 +22,48 @@ public class CharacterMovement : MonoBehaviour
         myRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
+    //FixedUpdate is better with physics
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        myRigidbody2D.velocity = new Vector2(moveInput * speed, myRigidbody2D.velocity.y);
+        if(lookright == false && moveInput > 0)
+        {
+            ChangeDirection();
+        }
+        else if(lookright == true && moveInput < 0)
+        {
+            ChangeDirection();
+        }
+            
+    }
+
     void Update()
     {
-        Movement();
         Jump();
     }
 
-    void Movement()
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            myRigidbody2D.velocity = new Vector2(myRigidbody2D.velocity.x, jumpForce);
+
+        }
+    }
+
+    void ChangeDirection()
+    {
+        lookright = false;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+
+
+    //old movement with sliding bug
+    /*void Movement()
     {
         //Check if player move right
         if (Input.GetAxisRaw("Horizontal") > 0f)
@@ -44,14 +80,7 @@ public class CharacterMovement : MonoBehaviour
         //    myRigidbody2D.velocity = new Vector2(0, myRigidbody2D.velocity.y);
         //}
     }
+    */
 
-    void Jump()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
-            myRigidbody2D.velocity = new Vector2(myRigidbody2D.velocity.x, jumpForce);
-
-        }
-    }
 
 }
---
+++ Project Major/Assets/TryoutFolder/TryoutScripts/PauseMenuScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    //public so that it's accessible for other scripts
+    public static bool PausedGame = false;
+    [SerializeField] private GameObject pauseUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PausedGame)
+                Resume();
+            else
+                Pause();
+        }
+
+    }
+
+    public void Resart()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Restart");
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("To Menu");
+        //SceneManager.LoadScene("Menu");
+    }
+
+    public void Resume()
+    {
+        //disable canvas
+        pauseUI.SetActive(false);
+
+        //unfreeze game
+        Time.timeScale = 1f;
+        PausedGame = false;
+    }
+
+    void Pause()
+    {
+        //enabled canvas
+        pauseUI.SetActive(true);
+
+        //freeze game
+        Time.timeScale = 0f;
+        PausedGame = true;
+    }
+
+    
+}
